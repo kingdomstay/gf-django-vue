0367d2019747c43dede6bc212c2f6abab0c29263
kingdomstay <kirill.troitskij@yandex.ru>
2023-10-12 06:19:24 +0300

    Удалены стартовые компоненты, добавлен vue router, главная страница, футер и навигационное меню.

---
+++ frontend/src/components/AppBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app-bar
+      app
+      color="brown"
+  >
+    <v-container>
+
+      <v-row
+          align="center"
+          justify="space-between"
+      >
+        <v-app-bar-title>
+          <h3 class="text-h4 white--text">G&F</h3>
+        </v-app-bar-title>
+        <div>
+          <v-btn class="deep-orange darken-1 white--text mx-1" depressed>
+            Главная
+          </v-btn>
+          <v-btn class="white black--text mx-1" depressed>
+            Меню
+          </v-btn>
+          <v-btn class="white black--text mx-1" depressed>
+            Личный кабинет
+          </v-btn>
+          <v-btn class="white black--text mx-1" depressed>
+            Контакты
+          </v-btn>
+        </div>
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,168 @@
+<template>
+  <div>
+    <v-carousel v-model="model">
+      <v-carousel-item
+          v-for="(color, i) in colors"
+          :key="color"
+      >
+        <v-sheet
+            :color="color"
+            height="100%"
+            tile
+        >
+          <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+          >
+            <div class="text-h2">
+              Slide {{ i + 1 }}
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+
+    <v-container>
+      <h2 class="text-h4 my-4">Сезонное меню</h2>
+      <v-row>
+        <v-col
+            cols="12"
+            sm="4"
+        >
+          <v-card>
+            <v-img
+                height="250"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Капучино</v-card-title>
+            <v-card-text>
+              <div>Итальянский напиток, состоящий из Эспрессо, слоя взбитого молока и слоя молочной пенки.</div>
+            </v-card-text>
+            <v-divider class="mx-4"></v-divider>
+            <v-card-text>
+              <v-chip class="orange darken-4 white--text"><h3>199 ₽</h3></v-chip>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col
+            cols="12"
+            sm="4"
+        >
+          <v-card>
+            <v-img
+                height="250"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Капучино</v-card-title>
+            <v-card-text>
+              <div>Итальянский напиток, состоящий из Эспрессо, слоя взбитого молока и слоя молочной пенки.</div>
+            </v-card-text>
+            <v-divider class="mx-4"></v-divider>
+            <v-card-text>
+              <v-chip class="orange darken-4 white--text"><h3>199 ₽</h3></v-chip>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col
+            cols="12"
+            sm="4"
+        >
+          <v-card>
+            <v-img
+                height="250"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Капучино</v-card-title>
+            <v-card-text>
+              <div>Итальянский напиток, состоящий из Эспрессо, слоя взбитого молока и слоя молочной пенки.</div>
+            </v-card-text>
+            <v-divider class="mx-4"></v-divider>
+            <v-card-text>
+              <v-chip class="orange darken-4 white--text"><h3>199 ₽</h3></v-chip>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container>
+      <h2 class="text-h4 my-4">Классическое меню</h2>
+      <v-row>
+        <v-col
+            cols="12"
+            sm="4"
+        >
+          <v-card>
+            <v-img
+                height="250"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Капучино</v-card-title>
+            <v-card-text>
+              <div>Итальянский напиток, состоящий из Эспрессо, слоя взбитого молока и слоя молочной пенки.</div>
+            </v-card-text>
+            <v-divider class="mx-4"></v-divider>
+            <v-card-text>
+              <v-chip class="orange darken-4 white--text"><h3>199 ₽</h3></v-chip>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col
+            cols="12"
+            sm="4"
+        >
+          <v-card>
+            <v-img
+                height="250"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Капучино</v-card-title>
+            <v-card-text>
+              <div>Итальянский напиток, состоящий из Эспрессо, слоя взбитого молока и слоя молочной пенки.</div>
+            </v-card-text>
+            <v-divider class="mx-4"></v-divider>
+            <v-card-text>
+              <v-chip class="orange darken-4 white--text"><h3>199 ₽</h3></v-chip>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col
+            cols="12"
+            sm="4"
+        >
+          <v-card>
+            <v-img
+                height="250"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Капучино</v-card-title>
+            <v-card-text>
+              <div>Итальянский напиток, состоящий из Эспрессо, слоя взбитого молока и слоя молочной пенки.</div>
+            </v-card-text>
+            <v-divider class="mx-4"></v-divider>
+            <v-card-text>
+              <v-chip class="orange darken-4 white--text"><h3>199 ₽</h3></v-chip>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: () => ({
+    model: 0,
+    colors: [
+      'primary',
+      'secondary',
+      'yellow darken-2',
+      'red',
+      'orange',
+    ],
+  }),
+}
+</script>
